c509be61e27952fdd152e2e40ff71ea26f17667f
jgpark <jjtjs159@gmail.com>
2021-05-23 22:42:16 +0900

    tsconfig.json, yarn package, controller

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { UpdateTodoDto } from './dto/update-todo.dto';
+import { TodoService } from './todo.service';
 
-@Controller('todo')
-export class TodoController {}
+@Controller('todos')
+export class TodoController {
+  constructor(private readonly service: TodoService) {}
+
+  @Get()
+  async index() {
+    return await this.service.findAll();
+  }
+
+  @Get(':id')
+  async find(@Param('id') id: string) {
+    return await this.service.findOne(id)
+  }
+
+  @Post(':id') 
+  async update(@Param('id') id: string, @Body() UpdateTodoDto: UpdateTodoDto) {
+    return await this.service.update(id, UpdateTodoDto);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    return await this.service.delete(id);
+  }
+}
